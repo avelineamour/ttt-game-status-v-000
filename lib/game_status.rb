113b1ac4704ebcb4f5826b21b83d9f0d0ab761ac
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-12-03 01:31:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,18 +16,6 @@ WIN_COMBINATIONS = [
   [6, 7, 8],
 ]
 
-# My Won Method
-# def won?(board)
-#   WIN_COMBINATIONS.any? do |wincombo|
-#     if  board[wincombo[0]] == board[wincombo[1]] &&
-#         board[wincombo[1]] == board[wincombo[2]] &&
-#         position_taken?(board, wincombo[1])
-#       return wincombo
-#     else false
-#     end
-#   end
-# end
-
 # Adopted Method
 def won?(board)
   WIN_COMBINATIONS.detect { |win_combination|
@@ -55,6 +43,5 @@ end
 def winner(board)
   if won?(board).is_a? Array
     board[won?(board)[0]]
-  else nil
     end
 end
